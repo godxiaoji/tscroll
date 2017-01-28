c4210f2a3d52eb50d91faf3a6c9424fd9b8d3305
Travis <linyongji@cmcm.com>
2017-01-28 22:11:36 +0800

    修复一些bug
    1. 增加滑动过程中对外参数
    2. 暂定取消按下轨道定向的问题

--- src/tscroll.js
+++ src/tscroll.js
@@ -2,7 +2,7 @@
  * tScroll
  * @Author  Travis(LinYongji)
  * @Contact http://travisup.com/
- * @Version 2.0.4a
+ * @Version 2.0.6
  * @date    2014-09-03
  */
 (function() {
@@ -21,28 +21,26 @@
             i = 0,
             m,
             match;
-        
-        if(selector.nodeType === 1) {
+
+        if (selector.nodeType === 1) {
             return selector;
         }
         match = rQuickExpr.exec(selector);
         if (match) {
             if ((m = match[1])) {
                 return doc.getElementById(m);
-            }
-            else {
+            } else {
                 // 获取父节点
                 parent = parent || doc;
                 if ((m = match[2])) {
                     rets = parent.getElementsByTagName(m);
-                }
-                else if ((m = match[3])) {
+                } else if ((m = match[3])) {
                     // class 支持nodes，被过滤节点
-                    if(!nodes && parent.getElementsByClassName) {
+                    if (!nodes && parent.getElementsByClassName) {
                         rets = parent.getElementsByClassName(m);
                     } else {
                         nodes = nodes || parent.getElementsByTagName('*');
-                        for(; i < nodes.length; i++) {
+                        for (; i < nodes.length; i++) {
                             if (nodes[i].nodeType === 1 && hasClass(nodes[i], m)) {
                                 rets.push(nodes[i]);
                                 break;
@@ -58,14 +56,17 @@
     function hasClass(elem, className) {
         return (' ' + elem.className + ' ').indexOf(' ' + className + ' ') >= 0;
     }
+
     function addClass(elem, className) {
         if (!hasClass(elem, className)) {
             elem.className = trim(elem.className + ' ' + className + ' ');
         }
     }
+
     function removeClass(elem, className) {
         elem.className = trim((' ' + elem.className + ' ').replace(' ' + className + ' ', ' '));
     }
+
     function setPixelCss(elem, name, val) {
         elem.style[name] = val + 'px';
     }
@@ -83,6 +84,7 @@
             elem['on' + type] = fn;
         }
     }
+
     function removeEvent(elem, type, fn) {
         if (elem.removeEventListener) {
             elem.removeEventListener(type, fn, false);
@@ -94,12 +96,15 @@
     }
     // 修复ie低版本事件
     function fixEvent(event) {
-        if(typeof event.pageX === 'undefined') {
+        if (typeof event.pageX === 'undefined') {
             event.pageX = event.clientX + (event && event.scrollLeft || doc.body.scrollLeft || 0);
             event.pageY = event.clientY + (event && event.scrollTop || doc.body.scrollTop || 0);
         }
-        event.preventDefault = event.preventDefault || function () {
+        event.preventDefault = event.preventDefault || function() {
             window.event.returnValue = false;
+        };
+        event.stopPropagation = event.stopPropagation || function() {
+            window.event.cancelBubble = true;
         };
         return event;
     }
@@ -113,7 +118,7 @@
     }
     // 批量插入子节点
     function append(parent, childNodes) {
-        while(childNodes[0]) {
+        while (childNodes[0]) {
             parent.appendChild(childNodes[0]);
         }
     }
@@ -121,31 +126,47 @@
     // 滚动条架构
     var frame = [
         '<div class="tscroll-view">',
-            '<div class="tscroll-pane">',
-                '{{content}}', // 内容
-            '</div>',
+        '<div class="tscroll-pane">',
+        '{{content}}', // 内容
+        '</div>',
         '</div>',
         '<div class="tscroll-bar tscroll-hide">',
-            '<div class="tscroll-track">',
-                '<div class="tscroll-thumb">',
-                    '<div class="tscroll-thumb-end">',
-                    '</div>',
-                '</div>',
-            '</div>',
+        '<div class="tscroll-track">',
+        '<div class="tscroll-thumb">',
+        '<div class="tscroll-thumb-end">',
+        '</div>',
+        '</div>',
+        '</div>',
         '</div>'
     ];
-    
+
     // 主模块
     function Scrollbar(eWrap, custom) {
         var self = this,
-            oView = {elem: getMatchElem('.tscroll-view', null, eWrap.childNodes)},
-            oPane = {elem: getMatchElem('.tscroll-pane', null, oView.elem.childNodes)},
-            oBar = {elem: getMatchElem('.tscroll-bar', null, eWrap.childNodes)},
-            oTrack = {elem: getMatchElem('.tscroll-track', oBar.elem)},
-            oThumb = {elem: getMatchElem('.tscroll-thumb', oBar.elem)},
+            oView = {
+                elem: getMatchElem('.tscroll-view', null, eWrap.childNodes)
+            },
+            oPane = {
+                elem: getMatchElem('.tscroll-pane', null, oView.elem.childNodes)
+            },
+            oBar = {
+                elem: getMatchElem('.tscroll-bar', null, eWrap.childNodes)
+            },
+            oTrack = {
+                elem: getMatchElem('.tscroll-track', oBar.elem)
+            },
+            oThumb = {
+                elem: getMatchElem('.tscroll-thumb', oBar.elem)
+            },
             iScroll = 0,
-            iPosition = {start: 0, now: 0},
-            iMouse = {start: 0, now: 0},
+            iPosition = {
+                start: 0,
+                now: 0
+            },
+            iMouse = {
+                start: 0,
+                now: 0
+            },
             sAxis = false,
             sDirection = 'top',
             sSize = 'Height',
@@ -161,24 +182,25 @@
                 axis: 'y', // 滚动方向
                 trackSize: 'auto', // 滚动条长度
                 thumbSize: 'auto', // 滑块长度
-                thumbMinSize: 10,  // 滑块最小长度
+                thumbMinSize: 10, // 滑块最小长度
                 lockScroll: true, // 锁定滚动事件不被外层接收
                 invertScroll: false, // 移动设备反转滚动
                 onSelect: true, // ie低版本的鼠标按下选择问题
                 bounce: false, // 位置反弹
-                callback: returnFalse // 滚动回调函数
+                callback: returnFalse, // 滚动回调函数
+                ready: returnFalse // 初始化完的调用函数
             };
 
         // 事件绑定
         function bindEvents() {
             // 拖动事件绑定
-            if(!touchEvents) {
+            if (!touchEvents) {
                 addEvent(oThumb.elem, 'mousedown', start);
-                addEvent(oTrack.elem, 'mouseup', drag);
+                //addEvent(oTrack.elem, 'mouseup', drag);
             } else {
                 // 触屏事件
-                oView.elem.ontouchstart = function(event) {   
-                    if(event.touches.length === 1) {
+                oView.elem.ontouchstart = function(event) {
+                    if (event.touches.length === 1) {
                         start(event.touches[0]);
                         event.stopPropagation();
                     }
@@ -186,9 +208,9 @@
             }
 
             // 滚动事件绑定
-            if(options.scroll) {
-                if(window.addEventListener ) {
-                    eWrap.addEventListener('mousewheel', wheel, false );
+            if (options.scroll) {
+                if (window.addEventListener) {
+                    eWrap.addEventListener('mousewheel', wheel, false);
                     // firefox事件独立开来
                     eWrap.addEventListener('DOMMouseScroll', wheel, false);
                     eWrap.addEventListener('MozMousePixelScroll', function(event) {
@@ -203,15 +225,17 @@
 
         // 滚动事件
         function wheel(event) {
-            if(oPane.ratio < 1) {
+            if (oPane.ratio < 1) {
                 var event = fixEvent(event || window.event),
-                    delta = event.wheelDelta ? event.wheelDelta / 120 : -(event.detail || 0) / 3;
+                    delta = event.wheelDelta ? event.wheelDelta / 120 : -(event.detail || 0) / 3,
+                    iScrollTemp = iScroll;
 
                 iScroll -= delta * options.wheel;
                 self.moveTo(iScroll);
 
-                if(options.lockScroll || (iScroll !== (oPane.slideSize) && iScroll !== 0 )) {
+                if (options.lockScroll || (iScroll !== oPane.slideSize && iScroll !== 0) || iScroll !== iScrollTemp) {
                     event.preventDefault();
+                    event.stopPropagation();
                 }
             }
         }
@@ -226,7 +250,7 @@
             iMouse.start = sAxis ? event.pageX : event.pageY;
             iPosition.start = isNaN(thumbPos) ? 0 : thumbPos;
 
-            if(!touchEvents) {
+            if (!touchEvents) {
                 addEvent(doc, 'mousemove', drag);
                 addEvent(doc, 'mouseup', end);
                 addEvent(oThumb.elem, 'mouseup', end);
@@ -241,10 +265,11 @@
         }
 
         function drag(event) {
-            if(oPane.ratio < 1) {
+            if (oPane.ratio < 1) {
                 var event = fixEvent(event || window.event);
                 iMouse.now = sAxis ? event.pageX : event.pageY;
-                if(options.invertscroll && touchEvents) {
+
+                if (options.invertscroll && touchEvents) {
                     // 触屏反转事件
                     iPosition.now = Math.min((oTrack.slideSize), Math.max(0, (iPosition.start + (iMouse.start - iMouse.now))));
                 } else {
@@ -257,7 +282,7 @@
             }
             // 解决由于ie拖出窗口丢失mouseup的问题
             var ua = window.navigator.userAgent.toLowerCase();
-            if(/msie/.test(ua) && !(document.documentMode >= 9) && !event.button) {
+            if (/msie/.test(ua) && !(document.documentMode >= 9) && !event.button) {
                 end.call(this, event);
             }
         }
@@ -266,7 +291,7 @@
             removeClass(body, 'tscroll-unselect');
             options.onSelect && (body.onselectstart = null);
 
-            if(!touchEvents) {
+            if (!touchEvents) {
                 removeEvent(doc, 'mousemove', drag);
                 removeEvent(doc, 'mouseup', end);
                 removeEvent(oThumb.elem, 'mouseup', end);
@@ -277,10 +302,10 @@
 
         // 回弹效果
         function rebound(origScroll) {
-            var speed = (origScroll == 0 ? options.wheel : -options.wheel)/1.2; //设置回弹速度
+            var speed = (origScroll == 0 ? options.wheel : -options.wheel) / 1.2; //设置回弹速度
 
             iScroll += speed;
-            if(iScroll >= 0 && iScroll <= oPane.slideSize) {
+            if (iScroll >= 0 && iScroll <= oPane.slideSize) {
                 iScroll = origScroll;
             }
             self.moveTo(iScroll);
@@ -289,6 +314,7 @@
         function initialize() {
             self.setting(custom);
             bindEvents();
+            options.ready(self);
             self.update();
         }
 
@@ -298,9 +324,9 @@
 
         this.setting = function(custom) {
             var i;
-            if(typeof custom === 'object') {
-                for(i in options) {
-                    if(typeof custom[i] !== 'undefined') {
+            if (typeof custom === 'object') {
+                for (i in options) {
+                    if (typeof custom[i] !== 'undefined') {
                         options[i] = custom[i];
                     }
                 }
@@ -316,14 +342,20 @@
 
         this.update = function(scroll) {
             var sCssSize = sSize.toLowerCase(),
-                thumbScrollSize = parseFloat(oThumb.elem.style[sDirection]);
+                thumbScrollSize = oThumb.elem.style[sDirection];
+
+            if (isNaN(parseFloat(thumbScrollSize))) {
+                thumbScrollSize = 0;
+            } else {
+                thumbScrollSize = parseFloat(thumbScrollSize);
+            }
 
             oView.size = oView.elem['offset' + sSize];
             oPane.size = oPane.elem['scroll' + sSize];
             oPane.slideSize = Math.max(oPane.size - oView.size, 0);
             oPane.ratio = oPane.size == 0 ? 1 : parseFloat(oView.size) / oPane.size;
 
-            if(oPane.ratio >= 1) {
+            if (oPane.ratio >= 1) {
                 addClass(oBar.elem, 'tscroll-hide');
             } else {
                 removeClass(oBar.elem, 'tscroll-hide');
@@ -335,10 +367,13 @@
 
             setPixelCss(oBar.elem, sCssSize, oTrack.size);
             setPixelCss(oTrack.elem, sCssSize, oTrack.size);
-            setPixelCss(oThumb.elem, sCssSize, oThumb.size);
-            if(typeof scroll !== 'undefined') {
+            setPixelCss(oThumb.elem, sCssSize, Math.round(oThumb.size));
+            if (typeof scroll !== 'undefined') {
                 iScroll = scroll === 'bottom' ? oPane.slideSize : parseInt(scroll);
             }
+
+            self.trackSize = oTrack.size;
+            self.thumbSize = oThumb.size;
 
             self.moveTo(iScroll);
             // 计算滚动条差值，修正滑动距离
@@ -351,9 +386,9 @@
             // 修正位置
             iScroll = Math.min((oPane.slideSize), Math.max(0, scroll));
             iScrollTemp = iScroll;
-            
+
             // 回弹效果
-            if(options.bounce && (scroll < 0 || scroll > oPane.slideSize)) {
+            if (options.bounce && (scroll < 0 || scroll > oPane.slideSize)) {
                 origScroll = iScroll;
 
                 clearTimeout(timer);
@@ -364,9 +399,12 @@
             }
 
             // 设置样式
-            setPixelCss(oThumb.elem, sDirection, iScrollTemp * oTrack.ratio);
+            setPixelCss(oThumb.elem, sDirection, Math.round(iScrollTemp * oTrack.ratio));
             setPixelCss(oPane.elem, sDirection, -iScroll);
+            
             self.scroll = iScroll;
+            self.scrollProgress = iScroll / oPane.slideSize;
+            self.scrollSize = oPane.slideSize;
             options.callback(self);
             return self;
         };
@@ -387,13 +425,13 @@
         touchEvents == null && (touchEvents = typeof doc.documentElement.ontouchstart !== "undefined");
 
         // 元素存在，才初始化
-        if(elem != null) {
+        if (elem != null) {
             // 如果已经被初始化，返回原有对象
             var old = elem.getAttribute("data-tscroll");
-            if(old && self.list[old]) {
+            if (old && self.list[old]) {
                 return self.list[old].update(0);
             }
-            
+
             addClass(elem, "tscroll");
             elem.setAttribute("data-tscroll", rand);
 
@@ -418,15 +456,15 @@
 
     window.tScroll = tScroll;
     // 支持amd和cmd
-    if ( typeof define === "function" ) {
-        if ( define.amd ) {
-            define( "tScroll", [], function () {
+    if (typeof define === "function") {
+        if (define.amd) {
+            define("tScroll", [], function() {
                 return tScroll;
-            } );
-        } else if ( define.cmd ) {
-            define( function (require, exports, module) {
+            });
+        } else if (define.cmd) {
+            define(function(require, exports, module) {
                 return tScroll;
-            } );
+            });
         }
     }
 })();
